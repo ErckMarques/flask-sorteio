2133b7218f5753077e3fdd15d59fa1b0bb44f36e
Erik Marques <lucro.alternativo@outlook.com>
2025-02-05 15:43:44 -0300

    Adicionada restrição 'if __name__ == __main__'

--- flask_sorteio/__init__.py
+++ flask_sorteio/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
-
 from .blueprints.home import bp as home_bp
 
 def create_app():
-
+    """Uma fábrica para a aplicação flask"""
     app = Flask(__name__)
     
     app.register_blueprint(home_bp)
     
-    return app
+    return app 
+
+if __name__ == '__main__':
+    app = create_app()
+
+    with app.app_context():
+        app.run(debug=True)
